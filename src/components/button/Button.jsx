ee5344e65a5de9cde51603c40c05855b1c2672a5
Tiara Temple <tmtemple7@gmail.com>
2023-01-17 14:02:33 -0600

    button component added

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = () => {
+  return <button>Add To Roster</button>;
+};
+
+export default Button;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Header.css";
 
-function Header() {
+const Header = () => {
   return (
     <header>
       <h1 className="header-title">NBA Fantasy</h1>
     </header>
   );
-}
+};
 
 export default Header;
