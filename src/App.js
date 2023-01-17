0e329e014c3a19b61c5c4a91561b13ddbf0b7b7b
Tiara Temple <tmtemple7@gmail.com>
2023-01-17 16:00:19 -0600

    axios request

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Card from "./components/Card/Card";
 import Form from "./components/form/Form";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.balldontlie.io/api/v1/players")
+      .then((response) => {
+        setPlayers(response);
+        console.log(response);
+      })
+      .catch((err) => console.log(err));
+  });
+
   return (
     <div className="App">
       <Header />
