1d6990e34475c87c9592a6fa592ce70cf47ae71d
Tiara Temple <tmtemple7@gmail.com>
2023-01-17 14:11:41 -0600

    imported btn to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,21 @@
-import './Card.css';
+import "./Card.css";
+import Button from "../button/Button";
+
 const Card = () => {
   return (
-    <div className='card-container'>
-      <div className='card'>
-        <span className='img-container'>
-          <img src="" alt="basketball player"/>
+    <div className="card-container">
+      <div className="card">
+        <span className="img-container">
+          <img src="" alt="basketball player" />
         </span>
         <p>Name</p>
         <p>Position</p>
         <p>Stats</p>
-        <div className='card-button-container'>
-          <button>Button Component goes here</button>
+        <div className="card-button-container">
+          <Button />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default Card;
