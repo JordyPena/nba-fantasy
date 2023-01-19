6f86f704b4fad16a14f5b8d6726ba30eeb3e321a
Tiara Temple <tmtemple7@gmail.com>
2023-01-18 19:48:14 -0600

    search for player functionality added

--- src/App.js
+++ src/App.js
@@ -2,27 +2,15 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Card from "./components/Card/Card";
 import Form from "./components/form/Form";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [players, setPlayers] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://www.balldontlie.io/api/v1/players")
-      .then((response) => {
-        setPlayers(response);
-        console.log(response);
-      })
-      .catch((err) => console.log(err));
-  });
-
+  const [player, setPlayer] = useState({});
   return (
     <div className="App">
       <Header />
-      <Form />
-      <Card />
+      <Form setPlayer={setPlayer} />
+      <Card player={player} />
       <Footer />
     </div>
   );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,22 @@
 import "./Card.css";
 import Button from "../button/Button";
 
-const Card = () => {
+const Card = ({ player }) => {
+  console.log("in card", player);
   return (
     <div className="card-container">
       <div className="card">
         <span className="img-container">
           <img src="" alt="basketball player" />
         </span>
-        <p>Name</p>
-        <p>Position</p>
-        <p>Stats</p>
+        {player && (
+          <>
+            <p>First Name: {player.first_name}</p>
+            <p>Last Name: {player.last_name}</p>
+            {player.position && <p>Position: {player.position}</p>}
+            <p>Team: {player.team?.full_name}</p>
+          </>
+        )}
         <div className="card-button-container">
           <Button buttonTxt="Add To Roster" />
         </div>
